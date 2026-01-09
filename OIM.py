55b840960df52a9f79d78638106d40eb378f5653
WZ（SII） <146328555+SII-WZ@users.noreply.github.com>
2026-01-10 00:16:38 +0800

    Add files via upload

--- OIM.py
+++ OIM.py
@@ -6,7 +6,7 @@ import os
 import matplotlib.pyplot as plt
 import copy
 import random
-
+import time
 
 def dirichlet_weighted_sq(num1, num2, data, weights=None):
 
@@ -38,17 +38,30 @@ def dirichlet_weighted3(num1, num2, num3, data, weights=None):
     return data[num1] * weights[0] + data[num2] * weights[1] + data[num3] * weights[2]
 
 
+def speckles_turn_200x200(imag_arry):
+    npy_img=[]
+    for i in range(imag_arry.shape[0]):
+        img_cv = imag_arry[i,]
+        img_new = cv.resize(img_cv, (200, 200), interpolation=cv.INTER_CUBIC)
+        #   print(img_cv)
+        # plt.imshow(img_new, cmap=plt.cm.gray)
+        # plt.show()
+        npy_img.append((np.array(copy.deepcopy(img_new))))
+    return np.array(npy_img)
+
 
 choose_num=200      #the number of all photo
-mix_nuw=2# the number of mix photo
+mix_nuw=3# the number of mix photo
 sq=0
 
 
-load_path="D:\zw\image_enhance\cifiar10_sq\\"
+load_path="D:\zw\image_enhance\cifiar10_sq\\" #your load_path
 
 
 y=np.load(load_path+"pattern.npy")
 x=np.load(load_path+"speckles.npy")
+
+x=speckles_turn_200x200(x)
 
 if sq == 1:
     sq_x = np.sqrt(x)
@@ -62,10 +75,10 @@ print(x.shape)
 conca_y = []
 conca_x = []
 
-
+start_time = time.time()
 for i in range(choose_num):
 
-    for o in range(20):
+    for o in range(100):
         num1 = random.randint(0, choose_num-1)
         num2 = random.randint(0, choose_num-1)
 
@@ -82,7 +95,8 @@ for i in range(choose_num):
             y_new = dirichlet_weighted3(i, num1, num2, y,weights)
             conca_y.append((np.array(copy.deepcopy(y_new))))
             conca_x.append((np.array(copy.deepcopy(x_new))))
-
+end_time = time.time()
+print(end_time - start_time)
 
 np.save(load_path+'conca_y_3bit', conca_y)
 np.save(load_path+'conca_x_3bit', conca_x)
